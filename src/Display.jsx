149cef3120897e28822eb8b3b04f2ac029674664
Cris Higham <cris@justblink.co.za>
2023-05-23 14:07:53 +0200

    Got context passing display

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,14 +1,15 @@
-
+import {useContext} from 'react';
+import {DisplayContext} from "./DisplayContext.js";
 
 function Display() {
-
-  return (
-    <>
-      <div className="bg-slate-400 my-2 rounded">
-          <p className="p-2 text-right">0.</p>
-      </div>
-    </>
-  )
+    const display = useContext(DisplayContext);
+      return (
+        <>
+          <div className="bg-slate-400 my-2 rounded">
+              <p className="p-2 text-right">{display}</p>
+          </div>
+        </>
+      )
 }
 
 export default Display
