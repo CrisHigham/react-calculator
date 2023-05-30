e24b3b0672695ad4a2dbaf7f3b33ae8d559887e2
Cris Higham <cris@justblink.co.za>
2023-05-30 20:15:16 +0200

    Feature: Added text area to avoid overflow

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,15 +1,27 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useRef} from 'react';
 import {StateContext} from "./CalculatorProvider.jsx";
 
 function Display() {
     const state = useContext(StateContext);
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+        }
+    }, [state.display]);
+
       return (
         <>
-          <div className="bg-slate-400 my-2 rounded">
-              <p className="p-2 text-right">{state.display}</p>
-          </div>
+              <textarea
+                  ref={textareaRef}
+                  className="resize-none overflow-hidden h-auto bg-slate-400 my-2 rounded p-2 text-right block w-full"
+                  value={state.display}
+                  disabled
+                  onChange={()=>{}}></textarea>
         </>
       )
 }
 
-export default Display
+export default Display;
