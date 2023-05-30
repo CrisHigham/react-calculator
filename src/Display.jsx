a89568f73550382cffb97468cf8223e735f80aea
Cris Higham <cris@justblink.co.za>
2023-05-30 20:19:41 +0200

    Feature: Added text area to avoid overflow

--- src/Display.jsx
+++ src/Display.jsx
@@ -12,16 +12,16 @@ function Display() {
         }
     }, [state.display]);
 
-      return (
-        <>
-              <textarea
-                  ref={textareaRef}
-                  className="resize-none overflow-hidden h-auto bg-slate-400 my-2 rounded p-2 text-right block w-full"
-                  value={state.display}
-                  disabled
-                  onChange={()=>{}}></textarea>
-        </>
-      )
+  return (
+    <>
+          <textarea
+              ref={textareaRef}
+              className="resize-none overflow-hidden h-auto bg-slate-400 my-2 rounded p-2 text-right w-full"
+              value={state.display}
+              disabled
+              onChange={()=>{}}></textarea>
+    </>
+  )
 }
 
 export default Display;
